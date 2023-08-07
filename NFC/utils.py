3198cb2f6e491c578235552b122f2df6695d30ab
Mohirbeck <mustaqimovmohir@gmail.com>
2023-08-07 14:06:03 +0500

    remove database and improve env

--- NFC/utils.py
+++ NFC/utils.py
@@ -8,4 +8,5 @@ CLIENT_URL: str = os.getenv('CLIENT_URL')
 SERVER_URL: str = os.getenv('SERVER_URL')
 ACCESS_TOKEN_LIFE_TIME_M: int = int(os.getenv('ACCESS_TOKEN_LIFE_TIME_M'))
 REFRESH_TOKEN_LIFE_TIME_H: int = int(os.getenv('REFRESH_TOKEN_LIFE_TIME_H'))
-
+ALLOWED_HOSTS: list = os.getenv('ALLOWED_HOSTS').split(',')
+DEBUG: bool = os.getenv('DEBUG') == 'True'
