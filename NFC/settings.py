7811f529d02a8d178ef50e4d561b788fe64afae8
Abdulaziz Bakhodirov <63644879+madfromthemars@users.noreply.github.com>
2023-08-12 16:43:56 +0500

    Update settings.py

--- NFC/settings.py
+++ NFC/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5t@r#bp&lcj5yxj^ehi@a-(diqhayi9(5$5&$$oehv&xk1zrcl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG
 
-# ALLOWED_HOSTS = ALLOWED_HOSTS
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ALLOWED_HOSTS
 
 # Application definition
 
